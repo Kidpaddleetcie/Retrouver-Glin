ee5237c9de02aea981af1e2f91f87991c0544139
KidpaddleetcieGlin <37848836+Kidpaddleetcie@users.noreply.github.com>
2020-06-04 21:58:30 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -25,7 +25,7 @@ const weblist = [{
     l: "https://sketchfab.com/KidpaddleetcieGlin/models",
     d: 'C\'est ici où je poste des modèles 3D gratuitement !'
 }, {
-    p: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAWCAQAAAAs/tcnAAABMUlEQVR4Ac3UJfQUARTGUaxnrBHpPeDu7u7uTg/QO/SK9q1U3KHg2i7fH4exNeSdc9/ZNL/RHfRfjdHOuOajt246b7Fh/T38WO/9OnfM7s+hx3gUh5XNWYN7D4wnTlOe6OGsv/6eQlQFmNX9WScTs4jqwD3DOr/j04gJxHyiOsCiTgITibnEdGIJURc410lgJrGYmE+sIuoCz1301BstGwxueqQLiVXEMmI9cVp7c9WIYmAqkUysIjYQa4mtRAJtJwb//kgXEdOJTcR2Yiuxi2g/wIYfgTnEBmIFsYvYS+whDhCdBFo/ApuJvcRO4hBxhDhMHCU6Cbz5EThKHCe+7BNEcXceKFxB/e7yFs0h1hP1u/OHXHiL6nfnr2nhO6jfR3r50L5cR9O+oX5+/FX8vfkElmh0Xu+KRLEAAAAASUVORK5CYII=",
+    p: "https://cdn2.iconfinder.com/data/icons/minimalism/512/soundcloud.png",
     t: "Le Soundcloud",
     l: "https://soundcloud.com/kidpaddleetcie-glin",
     d: 'Je m\y amuse à mettre en favoris les sons que j\'aime bien... parce que je n\'ai plus d\'espace mémoire de disponible sur le site ! '
@@ -54,6 +54,6 @@ const weblist = [{
 let generate = " ";
 for (let accu = 0; accu < weblist.length; accu++) {
     let wl = (weblist[accu]);
-    document.getElementById("capt").innerHTML += '<div class="media p-2"> <a href="' + wl.l + '"> <img src="' + wl.p + '" class="mr-3 align-self-center" width="50vh" alt="' + wl.t + '"> <div class="media-body"> <h5 class="mt-0">' + wl.t + '</h5> </a> ' + wl.d + '</div> </div> </div>';
+    document.getElementById("capt").innerHTML += '<div class="media p-2"> <a href="' + wl.l + '"> <img src="' + wl.p + '" class="mr-3 align-self-center shadow-lg" width="50vh" alt="' + wl.t + '"> <div class="media-body"> <h5 class="mt-0">' + wl.t + '</h5> </a> ' + wl.d + '</div> </div> </div>';
 
 }
